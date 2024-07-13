4a2aeb6a3d832984ee613ed8b617b3c5e520c5dd
nespinoza2000 <nespinozasegovia@gmail.com>
2024-07-12 22:47:48 -0400

    Update Frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
-import StickyNavbar from './components/header'; // Ajusta la ruta según la ubicación de tu archivo header.js
+// En App.jsx
+
+import React, { useState, useEffect } from 'react';
+import { StickyNavbar } from './components/header_a'; // Ajusta la ruta según la ubicación correcta de tu archivo header.jsx
+import { Loading } from './components/loading'; // Asegúrate de que la ruta sea correcta
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simula un proceso de carga (por ejemplo, una llamada a una API)
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000); // Cambia el tiempo según tus necesidades
+  }, []);
+
+  if (loading) {
+    return <Loading />; // Asegúrate de usar el componente Loading con la letra mayúscula
+  }
+
   return (
     <div>
       <StickyNavbar />
---
+++ src/components/loading.jsx
@@ -0,0 +1,10 @@
+import { Spinner } from "@material-tailwind/react";
+import React from 'react';
+
+export function Loading() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <Spinner className="h-16 w-16 animate-spin text-blue-900/50" />
+        </div>
+    );
+}
