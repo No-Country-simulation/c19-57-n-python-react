d1cb2f99f1c1483dbab9e26f0745d1524c3bc38b
lazaronazareno <lazaronazareno@gmail.com>
2024-07-12 22:54:20 -0300

    register form refactor and connected to backend, create utils.jsx for auxiliar functions. added env.example file

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,18 +1,30 @@
 import { useState } from 'react'
+import { validateRegisterForm } from '../utils'
 
-const userRoles = [
+const userGender = [
   'Seleccionar',
-  'Quiero adoptar',
-  'Soy un refugio',
-  'Soy de transito'
+  'femenino',
+  'masculino',
+  'no binario',
+  'prefiero no decirlo'
 ]
+
+const userCountries = ['Seleccionar', 'argentina', 'chile', 'colombia', 'peru']
+
+const API_URL = import.meta.env.VITE_API_URL
 
 const RegisterForm = () => {
   const [values, setValues] = useState({
     email: '',
     password: '',
     confirmPassword: '',
-    role: ''
+    name: '',
+    surname: '',
+    year: '',
+    phone: '',
+    gender: '',
+    country: '',
+    imgen_profile: ''
   })
 
   const [error, setError] = useState({})
@@ -24,55 +36,44 @@ const RegisterForm = () => {
     })
   }
 
-  const validateForm = () => {
-    let isValid = true
-    const newErrors = {}
-
-    if (values.email.trim() === '') {
-      newErrors.email = 'Introduce un email'
-      isValid = false
-    }
-
-    const validateEmail = (email) => {
-      const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
-      return regex.test(email)
-    }
-
-    if (!validateEmail(values.email)) {
-      newErrors.email = 'Introduce un email valido'
-      isValid = false
-    }
-
-    if (values.role === 'Seleccionar' || values.role === '') {
-      newErrors.role = 'Seleccione un rol'
-      isValid = false
-    }
-
-    if (values.password.trim() === '') {
-      newErrors.password = 'Introduce una contraseña'
-      isValid = false
-    }
-
-    if (values.confirmPassword.trim() === '') {
-      newErrors.confirmPassword = 'Introduce una contraseña'
-      isValid = false
-    }
-
-    if (values.password !== values.confirmPassword) {
-      newErrors.confirmPassword = 'Las contraseñas no coinciden'
-      isValid = false
-    }
-
-    setError(newErrors)
-    return isValid
-  }
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
 
-    if (validateForm()) {
-      //reemplazar por la conexion al back a crear un nuevo usuario
-      console.log(values)
+    const { year } = values
+
+    const parseYear = parseFloat(year)
+
+    /*   este formato 12/7/2024 - 22:35:04   
+    const create_at = `${date.toLocaleDateString()} - ${date.toLocaleTimeString()}`
+
+    o este: 2024-07-13T01:37:00.629Z
+     */
+    const create_at = new Date().toISOString()
+    console.log(create_at)
+
+    //sacar cuando actualicen el back
+    const body = JSON.stringify({
+      ...values,
+      id: 0,
+      year: parseYear,
+      create_at
+    })
+
+    if (validateRegisterForm(values, setError)) {
+      try {
+        console.log(values)
+        await fetch(`${API_URL}/register`, {
+          method: 'POST',
+          headers: {
+            accept: 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body
+        })
+      } catch (error) {
+        console.error(error)
+        setError({ ...error, apiError: error.message })
+      }
     } else {
       console.error('Ocurrio un error')
     }
@@ -81,6 +82,43 @@ const RegisterForm = () => {
   return (
     <form className='flex flex-col items-center gap-2' onSubmit={handleSubmit}>
       <h2>Formulario de Registro</h2>
+
+      <label>Nombre</label>
+      <input
+        type='text'
+        placeholder='Nombre'
+        value={values.name}
+        name='name'
+        onChange={handleChange}
+        className='rounded-md border-0 p-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600'
+      />
+      {error.name && <p className='mt-2 text-red-600 text-sm'>{error.name}</p>}
+
+      <label>Apellido</label>
+      <input
+        type='text'
+        placeholder='Apellido'
+        value={values.surname}
+        name='surname'
+        onChange={handleChange}
+        className='rounded-md border-0 p-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600'
+      />
+      {error.surname && (
+        <p className='mt-2 text-red-600 text-sm'>{error.surname}</p>
+      )}
+
+      <label>Imagen</label>
+      <input
+        type='text'
+        placeholder='url de imagen'
+        value={values.imgen_profile}
+        name='imgen_profile'
+        onChange={handleChange}
+        className='rounded-md border-0 p-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600'
+      />
+      {error.imgen_profile && (
+        <p className='mt-2 text-red-600 text-sm'>{error.imgen_profile}</p>
+      )}
 
       <label htmlFor='email'>Email</label>
       <input
@@ -95,19 +133,61 @@ const RegisterForm = () => {
         <p className='mt-2 text-red-600 text-sm'>{error.email}</p>
       )}
 
-      <label htmlFor='role'>¿Que quieres hacer?</label>
+      <label>Edad</label>
+      <input
+        type='number'
+        placeholder='18'
+        value={values.year}
+        name='year'
+        onChange={handleChange}
+        className='rounded-md border-0 p-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600'
+      />
+      {error.year && <p className='mt-2 text-red-600 text-sm'>{error.year}</p>}
+
+      <label htmlFor='gender'>Genero</label>
       <select
-        name='role'
+        name='gender'
         onChange={handleChange}
         className='rounded-md border-0 p-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600'
       >
-        {userRoles.map((item) => (
+        {userGender.map((item) => (
           <option key={item} value={item}>
-            {item}
+            {item.toUpperCase()}
           </option>
         ))}
       </select>
-      {error.role && <p className='mt-2 text-red-600 text-sm'>{error.role}</p>}
+      {error.gender && (
+        <p className='mt-2 text-red-600 text-sm'>{error.gender}</p>
+      )}
+
+      <label htmlFor='country'>Nacionalidad</label>
+      <select
+        name='country'
+        onChange={handleChange}
+        className='rounded-md border-0 p-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600'
+      >
+        {userCountries.map((item) => (
+          <option key={item} value={item}>
+            {item.toUpperCase()}
+          </option>
+        ))}
+      </select>
+      {error.country && (
+        <p className='mt-2 text-red-600 text-sm'>{error.country}</p>
+      )}
+
+      <label>Telefono</label>
+      <input
+        type='number'
+        placeholder='1112345678'
+        value={values.phone}
+        name='phone'
+        onChange={handleChange}
+        className='rounded-md border-0 p-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600'
+      />
+      {error.phone && (
+        <p className='mt-2 text-red-600 text-sm'>{error.phone}</p>
+      )}
 
       <label htmlFor='password'>Contraseña</label>
       <input
@@ -138,6 +218,10 @@ const RegisterForm = () => {
       <button className='rounded-md p-1.5 bg-blue-600 text-white hover:bg-blue-900'>
         Registrarse
       </button>
+
+      {error.apiError && (
+        <p className='mt-2 text-red-600 text-sm'>{error.apiError}</p>
+      )}
     </form>
   )
 }
