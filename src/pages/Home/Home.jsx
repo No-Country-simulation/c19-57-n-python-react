5d1ecbae365a8bf1ee1503335b54a0c886334d5b
Roy Andres <rhavila789@gmail.com>
2024-07-13 19:08:13 -0500

    chore: Update file paths and structure for Home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div id="home">
+      <div className="hero-container">
+        <div className="hero__img">
+          <img src="/woman.png" alt="Woman with your dog" />
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Home
