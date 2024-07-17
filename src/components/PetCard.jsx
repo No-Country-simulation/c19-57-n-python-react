4f63845f1972ff8786447e3f56a0aef649d0ac71
Roy Andres <rhavila789@gmail.com>
2024-07-16 22:57:24 -0500

    chore: Update PetCard component styling for better responsiveness

--- src/components/PetCard.jsx
+++ src/components/PetCard.jsx
@@ -1,14 +1,14 @@
 const PetCard = ({ nombre, sexo, edad, tamaño, caracter }) => {
   return (
-    <div className="w-[107px] rounded-[14px] overflow-hidden">
-      <div className="bg-red-200 h-[120px] w-full"></div>
-      <div className="px-2 text-[8px]">
-        <h2 className="text-center text-[12px] font-medium">{nombre}</h2>
+    <div className="w-full rounded-[14px] overflow-hidden shadow-[0_0_25px_0_#0000001A] pb-7">
+      <div className="bg-[#F9D7BD] h-[265px] w-full rounded-b-[14px]"></div>
+      <div className="px-7 text-[18px]">
+        <h2 className="text-center text-[18px] font-medium">{nombre}</h2>
         <p>Sexo: {sexo}</p>
         <p>Edad: {edad}</p>
         <p>Tamaño: {tamaño}</p>
         <p>Carácter: {caracter}</p>
-        <button>Adoptar</button>
+        <button className="text-sm font-semibold text-[#002140] bg-[#FDE4D1] rounded-[14px] py-2 w-full mt-4">Adoptar</button>
       </div>
     </div>
   )
