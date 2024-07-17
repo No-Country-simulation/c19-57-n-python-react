1aa9be54dfdfa136e77ca32d0f643f1a2380d592
Roy Andres <rhavila789@gmail.com>
2024-07-16 22:27:52 -0500

    feat: Add adoption category for dogs on home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,6 +8,13 @@ const Home = () => {
       <Hero />
       <div className='mx-[18px] relative'>
         <SearchBar />
+        <h2 className='pt-[62px] text-center text-[#002140]'>¿Qué queres adoptar?</h2>
+        <div className='rounded-[14px] overflow-hidden mt-[25px] shadow-[0_0_25px_0_#0000001A] pb-8'>
+          <div className='bg-[#D6E4E8]'>
+            <img src="/dog.png" alt="A dog" />
+          </div>
+          <h3 className='text-center text-[18px] pt-5'>Perros</h3>
+        </div>
       </div>
     </div>
   )
