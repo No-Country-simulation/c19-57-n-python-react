a78e406706f97123bb1b339cce09b4641bccc427
Roy Andres <rhavila789@gmail.com>
2024-07-23 22:28:03 -0500

    chore: Update PetCard component styling and responsiveness

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,22 +9,22 @@ const Home = () => {
         <SearchBar />
         <h2 className='pt-[62px] text-center text-[#002140] text-lg font-medium'>¿Qué queres adoptar?</h2>
         <div className='lg:container mx-auto'>
-          <div className='md:grid grid-flow-row grid-cols-2 gap-[18px]'>
-            <div className='rounded-[14px] overflow-hidden mt-[25px] shadow-[0_0_25px_0_#0000001A] pb-8'>
+          <div className='flex gap-[18px] justify-center w-full'>
+            <div className='rounded-[14px] overflow-hidden mt-[25px] shadow-[0_0_25px_0_#0000001A] pb-8 w-[583px] flex flex-col'>
               <div className='bg-[#D6E4E8]'>
-                <img className='mx-auto' src="/dog.png" alt="A dog" />
+                <img className='w-full object-cover' src="/dog.png" alt="A dog" />
               </div>
               <h3 className='text-center text-[18px] pt-5'>Perros</h3>
             </div>
-            <div className='rounded-[14px] overflow-hidden mt-[25px] shadow-[0_0_25px_0_#0000001A] pb-8'>
+            <div className='rounded-[14px] overflow-hidden mt-[25px] shadow-[0_0_25px_0_#0000001A] pb-8 w-[583px]'>
               <div className='bg-[#D6E4E8]'>
-                <img className='mx-auto' src="/dog.png" alt="A dog" />
+                <img className='w-full object-cover' src="/dog.png" alt="A dog" />
               </div>
               <h3 className='text-center text-[18px] pt-5'>Gatos</h3>
             </div>
           </div>
           <h2 className='text-lg text-center text-[#002140] py-7 font-medium'>Adopción urgente</h2>
-          <div className='md:grid md:grid-cols-2 md:grid-flow-row gap-5 lg:grid-cols-4'>
+          <div className='justify-center flex flex-col md:flex-row flex-wrap gap-4'>
             {
               pets.map((pet, index) => (
                 <PetCard
