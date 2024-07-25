fb5234199e499fab734b94bd4e818f768979f0a7
ecasallas07 <ecasallas07@gmail.com>
2024-07-25 16:40:01 -0500

    fix:resolve problem with Exception

--- Backend/routers/user_create_router.py
+++ Backend/routers/user_create_router.py
@@ -1,6 +1,6 @@
-# from typing import Annotated
+
 from fastapi import APIRouter, HTTPException, Depends
-from fastapi.security import HTTPAuthorizationCredentials, OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from Backend.schemas.scheme_user import create_user, login_user, user_in, Token, TokenData
 from Backend.config.data_base import localsesion
@@ -13,7 +13,11 @@ from datetime import datetime, timedelta, timezone
 import jwt
 from pydantic import ValidationError
 from jwt.exceptions import InvalidTokenError
+import os
+from dotenv import load_dotenv
 
+# Loading environment variables
+load_dotenv()
 
 
 user_model.base.metadata.create_all(bind=engine)
@@ -30,9 +34,9 @@ def get_db():
         
         
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv('ALGORITHM')
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv('ACCESS_TOKEN_EXPIRE_MINUT')
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
