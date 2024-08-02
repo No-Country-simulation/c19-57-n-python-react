d80c144f38439b3f8da401c1bd206edf7ed31570
Roy Andres <rhavila789@gmail.com>
2024-08-01 19:57:00 -0500

    chore: Update PetDetails page layout and styles

--- src/pages/PetDetails.jsx
+++ src/pages/PetDetails.jsx
@@ -30,28 +30,44 @@ const PetDetails = () => {
 
   return (
     pet && (
-      <div id="PetDetails">
+      <div className="mx-auto lg:max-w-[1216px]" id="PetDetails">
         <div className="w-full bg-pink-default py-[30px] px-[20px] flex items-center place-content-between">
           <h1 className="text-left text-2xl font-semibold">{pet.name}</h1>
-          <div className="bg-white p-[6.4px] rounded-md">
-            <img src="/heart.svg" alt="" />
-          </div>
         </div>
-        <div className="shadow-[0px_7px_25px_-9px_#00000040] rounded-2xl">
-          <div className="overflow-hidden rounded-b-2xl mb-[34px]">
-            <img src={`${IMG_FOLDER_URL}/perfil/${pet.imagen_profile}`} alt="Pet image" />
+        <div className="shadow-[0px_7px_25px_-9px_#00000040] rounded-2xl md:flex md:mb-[43px] items-center">
+          <div className="overflow-hidden rounded-b-2xl mb-[34px] md:mb-0 md:w-1/2">
+            <img className="m-auto" src={`${IMG_FOLDER_URL}/perfil/${pet.imagen_profile}`} alt="Pet image" />
           </div>
-          <div className="capitalize flex w-full place-content-between px-[20px] pb-5 mb-6 text-[#516746]">
-            <div>{pet.gender}</div>
-            <div>{pet.characteristics}</div>
-            <div>{pet.size} cm</div>
-            <div>{pet.year} años</div>
+          <div className="capitalize flex w-full px-[20px] pb-5 mb-6 md:mb-0 md:flex-col md:w-1/2 md:pr-[50px]">
+            <div className="md:space-y-3 md:ml-[29px] md:mb-[25px] flex place-content-between w-full md:block">
+              <p className="font-semibold flex flex-col items-center text-[14px] md:text-[20px] gap-1 md:flex-row md:font-normal">
+                <img className="md:inline mr-2" src="/gender.svg" alt="Gender Icon" />
+                <span className="md:font-semibold uppercase text-[10px] md:text-xl">Sexo<span className="hidden md:inline">:</span> </span>
+                {pet.gender}
+              </p>
+              <p className="font-semibold flex flex-col items-center text-[14px] md:text-[20px] gap-1 md:flex-row md:font-normal">
+                <img className="md:inline mr-2" src="/caracter.svg" alt="Caracter Icon" />
+                <span className="md:font-semibold uppercase text-[10px] md:text-xl">Carácter<span className="hidden md:inline">:</span> </span>
+                {pet.characteristics}
+              </p>
+              <p className="font-semibold flex flex-col items-center text-[14px] md:text-[20px] gap-1 md:flex-row md:font-normal">
+                <img className="md:inline mr-2" src="/size.svg" alt="Size Icon" />
+                <span className="md:font-semibold uppercase text-[10px] md:text-xl">Tamaño<span className="hidden md:inline">:</span> </span>
+                {pet.size} cm
+              </p>
+              <p className="font-semibold flex flex-col items-center text-[14px] md:text-[20px] gap-1 md:flex-row md:font-normal">
+                <img className="md:inline mr-2" src="/years.svg" alt="Years Icon" />
+                <span className="md:font-semibold uppercase text-[10px] md:text-xl">Edad<span className="hidden md:inline">:</span> </span>
+                {pet.year} años
+              </p>
+            </div>
+            <button className="hidden md:block bg-pink-default font-semibold py-2 w-full rounded-2xl">Quiero adoptar</button>
           </div>
         </div>
         <div className="px-5 mb-[38px]">
-          <h2 className="mb-3 font-medium text-[#516746]">Detalle</h2>
+          <h2 className="mb-3 font-medium text-blue-darker">Detalle</h2>
           <p className="mb-[38px] text-blue-darker">{pet.history}</p>
-          <button className="bg-pink-default font-semibold py-2 w-full rounded-2xl">Quiero adoptar</button>
+          <button className="md:hidden bg-pink-default font-semibold py-2 w-full rounded-2xl">Quiero adoptar</button>
         </div>
         <h2 className="font-medium text-xl text-center mb-[44px]">Mascotas relacionadas</h2>
         <ListPets />
