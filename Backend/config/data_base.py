18cb1a421134e73ff3dcea084caa7ddda1359cdb
lazaronazareno <lazaronazareno@gmail.com>
2024-08-06 23:58:16 -0300

    docker, script data for database, static files in main, fix connection close()

--- Backend/config/data_base.py
+++ Backend/config/data_base.py
@@ -9,8 +9,10 @@ load_dotenv()
 MYSQL_USER = os.getenv('MYSQL_USER')
 MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
 MYSQL_DB = os.getenv('MYSQL_DB')
+MYSQL_HOST = os.getenv('MYSQL_HOST')
+MYSQL_PORT = os.getenv('MYSQL_PORT')
 
-engine = create_engine(f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@localhost/{MYSQL_DB}")
+engine = create_engine(f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}")
 
 localsesion = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
